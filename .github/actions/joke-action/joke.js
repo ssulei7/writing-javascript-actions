4c91cf584fefc38c6c05838fefbba57af9eba454
Suleiman Suleiman <ssulei7@github.com>
2023-01-17 10:49:08 -0600

    creating joke.js main.js

---
+++ .github/actions/joke-action/joke.js
@@ -0,0 +1,18 @@
+const request = require("request-promise")
+
+const options = {
+    method: "GET",
+    uri: "https://icanhazdadjoke.com/",
+    headers: {
+        "Accept": "application/json",
+        "User-Agent": "Writing Javascript action GitHub Skills Course."
+    },
+    json: true
+}
+
+async function getJoke() {
+    const res = await request(options);
+    return res.joke;
+}
+
+module.exports = getJoke;
